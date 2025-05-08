7e223eda872e99eaa4f2f1836e44f3f937da05f3
Alex Chiabai <chia0124@algonquinlive.com>
2025-05-08 13:47:28 -0400

    JavaScript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,11 @@
-alert("hello world!")
+//the alert function will add a pop-up with a message
+//alert("hello world!")
+
+console.log("hello world! ...quietly");
+
+//tell the first paragraph (id p1) to listen for a click
+document.getElementById("p1").addEventListener
+("click", function(){
+        //change the text in the second paragraph (id p2)
+        document.getElementById("p2").textContent = "You clicked on the first paragraph";
+});
